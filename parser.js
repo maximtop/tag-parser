d0831cf38ce67e0b2bac8d0d3828f4fe50a16a37
Maxim Topciu <maximtop@gmail.com>
2020-06-17 21:10:11 +0300

    make parser with stack and finite state automata

--- parser.js
+++ parser.js
@@ -1,44 +1,114 @@
-const util = require('util')
+const util = require('util');
 
-const log = (obj) => {
-    console.log(util.inspect(obj, {showHidden: false, depth: null}));
-};
+const STATES = {
+    TEXT: 'text',
+    OPEN_TAG: 'open_tag',
+    CLOSE_TAG: 'close_tag',
+}
 
-const parseString = (str) => {
-    const tagOpeningBraceIdx = str.indexOf('<');
+const textNode = (str) => {
+    return { type: 'text', value: str };
+}
 
-    if (tagOpeningBraceIdx > -1) {
-        let tagClosingBraceIdx = str.indexOf('>', tagOpeningBraceIdx);
-        let tagName = str.substring(tagOpeningBraceIdx + 1, tagClosingBraceIdx);
-        let closingTag = `</${tagName}>`;
-        let closingTagIdx = str.lastIndexOf(closingTag);
-        let tagContent = str.substring(tagClosingBraceIdx + 1, closingTagIdx);
-        const tagNode = {
-            type: 'tag',
-            value: tagName,
-            children: [ ...parseString(tagContent)],
-        }
-        const contentBeforeTag = str.substring(0, tagOpeningBraceIdx);
-        const nodeBeforeTag = parseString(contentBeforeTag);
-        const contentAfterTag = str.substring(closingTagIdx + closingTag.length);
-        const nodeAfterTag = parseString(contentAfterTag);
-        return [...nodeBeforeTag, tagNode, ...nodeAfterTag];
+const tagNode = (tagName, children) => {
+    if (children && children.length > 0) {
+        return { type: 'tag', value: tagName, children};
     }
+    return { type: 'tag', value: tagName }
+}
 
-    if (str.length > 0) {
-        const textNode = {
-            type: 'text',
-            value: str,
-        }
-
-        return [textNode];
-    }
-
-    return [];
+const isNode = (checked) => {
+    return !!checked?.type;
 }
 
 const parser = (str) => {
-    return parseString(str);
+    const stack = [];
+    const result = [];
+
+    let i = 0;
+    let currentState = STATES.TEXT;
+    let openTag = '';
+    let closeTag = '';
+    let text = '';
+
+    while (i < str.length) {
+        const currChar = str[i];
+        switch (currentState) {
+            case STATES.TEXT: {
+                if (currChar === '<') {
+                    currentState = STATES.OPEN_TAG;
+                    if (text.length > 0) {
+                        const node = textNode(text);
+                        if (stack.length > 0) {
+                            stack.push(node);
+                        } else {
+                            result.push(node);
+                        }
+                        text = '';
+                    }
+                } else {
+                    text += currChar;
+                }
+                break;
+            }
+            case STATES.OPEN_TAG: {
+                if (currChar === '>') {
+                    currentState = STATES.TEXT;
+                    stack.push(openTag);
+                    openTag = '';
+                } else if (currChar === '/') {
+                    currentState = STATES.CLOSE_TAG;
+                } else {
+                    openTag += currChar;
+                }
+                break;
+            }
+            case STATES.CLOSE_TAG: {
+                if (currChar === '>') {
+                    currentState = STATES.TEXT;
+                    let children = [];
+                    while (true) {
+                        const lastFromStack = stack.pop();
+                        if (lastFromStack === closeTag) {
+                            const node = tagNode(closeTag, children);
+                            if (stack.length > 0) {
+                                stack.push(node);
+                            } else {
+                                result.push(node);
+                            }
+                            children = [];
+                            break;
+                        } else {
+                            if (isNode(lastFromStack)) {
+                                children.unshift(lastFromStack);
+                            } else {
+                                throw new Error('String has unbalanced tags')
+                            }
+                        }
+                        if (stack.length === 0 && children.length > 0) {
+                            throw new Error('String has unbalanced tags');
+                        }
+                    }
+                    closeTag = '';
+                } else {
+                    closeTag += currChar;
+                }
+                break;
+            }
+        }
+
+        i += 1;
+    }
+
+    if (text.length > 0) {
+        result.push(textNode(text))
+    }
+
+    if (stack.length > 0) {
+        throw new Error('String has unbalanced tags');
+    }
+
+    return result;
 }
 
 module.exports.parser = parser;
